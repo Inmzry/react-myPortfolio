30c9a7c6c3cc25a42aeea49da545272b829bb663
Your Name <you@example.com>
2023-08-09 22:57:13 +0800

    minor changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -50,7 +50,7 @@ const SocialLinks = () => {
         <ul>
             {links.map(({ id, child, href, style, download }) => (
                 <li key={id} 
-                className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'+ " " +style}>
+                className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style}`}>
                 <a href={href} 
                 className='flex justify-between items-center w-full text-white'
                 download={download}
