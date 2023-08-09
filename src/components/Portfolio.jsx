1444083c5308e869aef257335a8118cb8d83c271
Your Name <you@example.com>
2023-08-09 22:59:28 +0800

    minor changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -33,7 +33,7 @@ const Portfolio = () => {
     <div name='portfolio' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Porfolio</p>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
                 <p className='py-6'>Check out some of my projects here</p>
             </div>
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
