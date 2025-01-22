cf5d3bed9324ffe4142edb01a711cb8bf776e398
inmzry <vinz_voilent@yahoo.com>
2025-01-22 18:55:29 +0800

    Portfolio Update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import portfolioReact from '../assets/portfolio/portfolio.jpg';
-import installNode from '../assets/portfolio/installNode.jpg';
 import navbar from '../assets/portfolio/navbar.jpg';
 import reactParallax from '../assets/portfolio/reactParallax.jpg';
 import reactSmooth from '../assets/portfolio/reactSmooth.jpg';
